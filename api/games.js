f3e7895eeae12b356789883eca447157c2e7569f
edwall70 <edwall7044@gmail.com>
2025-12-28 13:04:40 -0500

    Add CORS headers to API

--- api/games.js
+++ api/games.js
@@ -1,4 +1,10 @@
 export default async function handler(req, res) {
+    // Enable CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  
   if (req.query.action !== 'games') {
     return res.status(404).json({ error: 'Not found' });
   }
